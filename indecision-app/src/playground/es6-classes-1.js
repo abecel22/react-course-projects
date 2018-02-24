70b010ecb4e46082dff93f0b1cefd1fbcd2e2340
abecel22 <abe2211@gmail.com>
2018-02-24 15:47:52 -0600

    Changed app.js to inlude components.

--- indecision-app/src/playground/es6-classes-1.js
+++ indecision-app/src/playground/es6-classes-1.js
@@ -11,9 +11,40 @@ class Person {
     }
 }
 
-const me = new Person('Abel Acosta', 36);
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age);
+        this.major = major;
+    }
+    hasMajor() {
+        return !!this.major;
+    }
+    getDescription() {
+        let description = super.getDescription();
+        if (this.hasMajor()) {
+            description += `Their major is ${this.major}.`;
+        }
+        return description;
+    }
+}
 
-console.log(me.getDescription());
+class Traveler extends Person {
+    constructor(name, age, homeLocation) {
+        super(name, age);
+        this.homeLocation = homeLocation;
+    }
+    getGreeting() {
+        let greeting = super.getGreeting();
+        if (this.homeLocation) {
+            greeting += `I'm visiting from ${this.homeLocation}`;
+        }
+        return greeting;
+    }
+}
 
-const other = new Person();
-console.log(other.getDescription());
+const me = new Traveler('Abel Acosta', 36, 'Houston');
+
+console.log(me.getGreeting());
+
+const other = new Traveler();
+console.log(other.getGreeting());
