01958aceebc8b9239d3fcbc8897634d0c7b4ad67
abecel22 <abe2211@gmail.com>
2018-03-05 22:51:53 -0600

    added default and named imports

--- indecision-app/public/bundle.js
+++ indecision-app/public/bundle.js
@@ -60,42 +60,16 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 16);
 /******/ })
 /************************************************************************/
-/******/ ([
-/* 0 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/******/ ({
 
-"use strict";
-Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__person_js__ = __webpack_require__(1);
-//import './utils.js';
+/***/ 16:
+/***/ (function(module, exports) {
 
-// import { square, add } from './utils.js';
-
-// console.log('app.js is running');
-// console.log(square(4));
-// console.log(add(100, 23));
-
-
-
-console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["b" /* isAdult */](17));
-console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["a" /* canDrink */](21));
-
-
-/***/ }),
-/* 1 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "b", function() { return isAdult; });
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return canDrink; });
-const isAdult = (age) => age >= 18;
-const canDrink = (age) => age >= 21;
-
-
-
+throw new Error("Module parse failed: /Users/acosta/Documents/Github/react-course-projects/indecision-app/src/app.js Unexpected token (4:17)\nYou may need an appropriate loader to handle this file type.\n| import ReactDom from 'react-dom';\n| \n| const template = <p>testing 123</p>;\n| ReactDom(template, document.getElementById('app'));\n| ");
 
 /***/ })
-/******/ ]);
+
+/******/ });
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,12 +1,5 @@
-//import './utils.js';
+import React from 'react';
+import ReactDom from 'react-dom';
 
-// import { square, add } from './utils.js';
-
-// console.log('app.js is running');
-// console.log(square(4));
-// console.log(add(100, 23));
-
-import { isAdult, canDrink } from './person.js';
-
-console.log(isAdult(17));
-console.log(canDrink(21));
+const template = <p>testing 123</p>;
+ReactDom(template, document.getElementById('app'));
